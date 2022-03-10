94c67401c6d3dfadab9554843c2b1747b00a1543
mamconake <naftalikipkemboi@gmail.com>
2022-03-10 14:00:48 -0800

    A C program that prints using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * Description: The function print some text to the stdout
  * Return: returns void
 */
-int main ()
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	
-	return 0;
+	puts('"Programming is like building a multilingual puzzle\n');
+	return(0);
 }
