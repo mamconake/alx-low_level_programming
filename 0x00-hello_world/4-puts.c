8dfe0228e16797a6070297eb0cd1c2a08bbd8345
mamconake <naftalikipkemboi@gmail.com>
2022-03-10 13:46:47 -0800

    A C program that prints using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: The function print some text to the stdout
+ * Return: returns void
+*/
+int main ()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return 0;
+}
