66fefcfb1e1923ca85d85f03280bf86768207ace
mamconake <naftalikipkemboi@gmail.com>
2022-03-12 04:05:01 -0800

    Adding long int and long long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
+	long int longintTyype;
+	long long int longlongintType;
 	char charType;
-
-	//sizeof evaluates the size of a variable
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
 	printf("Size of char: %zu bytes\n", sizeof(charType));
 	return (0);
 }
