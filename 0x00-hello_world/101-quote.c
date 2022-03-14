7c9385ca1fafa11e6804cb50f7369deab033b0f9
mamconake <naftalikipkemboi@gmail.com>
2022-03-14 16:10:12 -0700

    Updating documentation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- ** main -entry point
- ** description - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error"
- ** return (1)
+ ** main - Prints a quote
+ ** Return: Always 0 (Success)
  **/
-int main (void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
