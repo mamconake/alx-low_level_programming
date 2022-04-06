a78767d311811a96df473f54cd4c32f454b02f1b
mamconake <naftalikipkemboi@gmail.com>
2022-04-06 15:25:00 -0700

    Updating liball.a

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,7 +8,7 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
+	int i, sign = 1, resp = 0, firstNum;
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
@@ -17,8 +17,7 @@ int _atoi(char *s)
 			sign *= -1;
 		}
 	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	for ( i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
