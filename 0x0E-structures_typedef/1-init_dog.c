30741a7f9730f2810ae847da257dd9d11f2d4e9c
mamconake <naftalikipkemboi@gmail.com>
2022-04-12 10:02:18 -0700

    Checking whether the pointer is NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  ** init_dog - initializes a variable of type struct dog
  ** @d: address of a struct dog
@@ -8,6 +9,8 @@
  **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
